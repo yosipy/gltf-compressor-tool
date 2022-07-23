66d8b079e80f73842472015990d24dc41380cf0d
yosipy <todacchi.desu@gmail.com>
2022-07-24 03:40:27 +0900

    workディレクトリにSeparateGltfを出力

--- index.js
+++ index.js
@@ -1,18 +1,78 @@
-console.log("########## start ##########");
+const fs = require("fs-extra");
+const inputDir = "./input";
+const outputDir = "./output";
+const workDir = "./work";
 
-const fs = require("fs");
-const input_dir = "./input";
+const gltfPipeline = require("gltf-pipeline");
+const fsExtra = require("fs-extra");
 
-fs.readdir(input_dir, (err, fileNames) => {
+function rmdir(dir) {
+  return new Promise((resolve) => {
+    fs.remove(dir, (err) => {
+      resolve(err);
+    });
+  });
+}
+
+function mkdir(dir) {
+  return new Promise((resolve) => {
+    fs.mkdir(dir, (err) => {
+      resolve(err);
+    });
+  });
+}
+
+const glbToGltf = async (inputDir, outputDir, fileName) => {
+  const glb = fsExtra.readFileSync(`${inputDir}/${fileName}`);
+  const results = await gltfPipeline.glbToGltf(glb);
+  await fsExtra.writeJsonSync(
+    `${outputDir}/${fileName.replace(".glb", ".gltf")}`,
+    results.gltf
+  );
+};
+
+const gltfToSeparateGltf = async (inputDir, outputDir, fileName) => {
+  const processGltf = gltfPipeline.processGltf;
+  const gltf = fsExtra.readJsonSync(`${inputDir}/${fileName}`);
+  const options = {
+    separateTextures: true,
+  };
+  const results = await processGltf(gltf, options);
+  await fsExtra.writeJsonSync(`${outputDir}/${fileName}`, results.gltf);
+  // Save separate resources
+  const separateResources = results.separateResources;
+  for (const relativePath in separateResources) {
+    if (separateResources.hasOwnProperty(relativePath)) {
+      const resource = separateResources[relativePath];
+      await fsExtra.writeFileSync(`${outputDir}/${relativePath}`, resource);
+    }
+  }
+};
+
+const main = async () => {
+  console.log("########## start ##########");
+
+  const fileNames = await fs.readdir(inputDir);
   console.log(fileNames);
-  if (err) throw err;
+
   const glbFileNames = fileNames.filter((fileName) => {
     return (
-      fs.statSync(`${input_dir}/${fileName}`).isFile() &&
+      fs.statSync(`${inputDir}/${fileName}`).isFile() &&
       /.*\.glb$/.test(fileName)
     );
   });
+  glbFileNames.forEach(async (glbFileName) => {
+    await rmdir(workDir);
+    await mkdir(workDir);
+    await glbToGltf(inputDir, workDir, glbFileName);
+    await gltfToSeparateGltf(
+      workDir,
+      workDir,
+      glbFileName.replace(".glb", ".gltf")
+    );
+  });
   console.log(glbFileNames);
-});
+  console.log("########## end ##########");
+};
 
-console.log("########## end ##########");
+main();
